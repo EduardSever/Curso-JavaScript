542685b958964c3f09083723d16de528ea8aa811
EduardServer <eduaarz24@gmail.com>
2023-05-23 17:50:33 -0600

    Subiendo avance del curso

---
+++ Ejemplos/VarOper.js
@@ -0,0 +1,59 @@
+
+/*Ejemplo de variable*/
+/*recipiente="papel";
+alert(recipiente)*/
+
+/*Imprimir una variable*/
+/*let nombre = prompt("Dime tu nombre");
+alert("Hola buenos dias " + nombre);*/
+
+/*Operadores de variables*/
+/*let numero=10;
+numero -=5;
+document.write(numero);*/
+
+/*Operando resto */
+/*let numero=50;
+numero %=6;
+document.write(numero);*/
+
+/*exponencialidad*/
+/*let numero=10;
+numero **=2;
+document.write(numero);*/
+
+/*Operdores aritmeticos Imprime negativo*/
+/*numero1 = 10;
+numero2 = 5;
+resultado = -numero1;
+alert(resultado);*/
+
+/*Concatenación*/
+/*saludo = " ¡Hola pedro! ";
+pregunta= " ¿Como estas? ";
+respuesta=saludo+pregunta;
+document.write(respuesta);*/
+/*Contatenación de Strings and numbers*/
+/*numero1 = "53";
+numero2 = 8;
+frase = numero1.concat(numero2);
+document.write(frase);*/
+
+/*Concatenación*/
+nombre = "Jose Perez";
+frase = " Soy "+nombre+" y estoy caminando";
+document.write(frase);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
